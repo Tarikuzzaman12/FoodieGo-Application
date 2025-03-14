ffa06b8d204e5fdb174fc0ba79ef8812dbb72baa
mithun950 <mithunrony46@gmail.com>
2025-03-14 15:50:28 +0600

    error handle

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,15 @@
-<<<<<<< HEAD
 import AreaCards from "@/components/AreaCards";
-=======
 
 import HeroSection from "@/components/HeroSection";
->>>>>>> be7b87f15a50e9e317367400a9cb1494027a8b21
 import Image from "next/image";
 
 export default function Home() {
   return (
    <div>
-   <p className="text-center text-5xl">FoodieGo-Application</p>
-<<<<<<< HEAD
-   <AreaCards></AreaCards>
-   <p className="text-center text-5xl">FoodieGo-Application by Tech Titans</p>
-=======
+   
    <HeroSection></HeroSection>
->>>>>>> be7b87f15a50e9e317367400a9cb1494027a8b21
+   <AreaCards></AreaCards>
+   
    </div>
   );
 }
